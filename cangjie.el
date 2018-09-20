10c97399e5718419aa0a98e0df40cb02a6b832ea
Kisaragi Hiu <mail@kisaragi-hiu.com>
2018-09-20 23:46:14 +0800

    Move abc-to-han-hash into abc-to-han function definition

--- cangjie.el
+++ cangjie.el
@@ -83,23 +83,20 @@ Grab lines from FILE containing S."
        (cangjie--file-contains? val "name:")
        (cangjie--file-contains? val "use_preset_vocabulary:")))
 
-(defconst cangjie--abc-to-han-hash
-  #s(hash-table
-     size 60
-     test equal
-     data ("a" "日" "b" "月" "c" "金" "d" "木" "e" "水" "f" "火"
-           "g" "土" "h" "竹" "i" "戈" "j" "十" "k" "大" "l" "中"
-           "m" "一" "n" "弓" "o" "人" "p" "心" "q" "手" "r" "口"
-           "s" "尸" "t" "廿" "u" "山" "v" "女" "w" "田" "x" "難"
-           "y" "卜" "z" "重"))
-  "Hash table mapping alphabetical Canjie representation to Han character representation.")
-
 (defun cangjie--abc-to-han (abc)
   "Convert alphabetical Cangjie code representation ABC into Han characters."
-  (->> (downcase abc)
-       (s-split "")
-       (--map (gethash it cangjie--abc-to-han-hash))
-       (s-join "")))
+  (let ((h #s(hash-table
+	      size 52
+	      test equal
+	      data ("a" "日" "b" "月" "c" "金" "d" "木" "e" "水" "f" "火"
+		    "g" "土" "h" "竹" "i" "戈" "j" "十" "k" "大" "l" "中"
+		    "m" "一" "n" "弓" "o" "人" "p" "心" "q" "手" "r" "口"
+		    "s" "尸" "t" "廿" "u" "山" "v" "女" "w" "田" "x" "難"
+		    "y" "卜" "z" "重"))))
+    (->> (downcase abc)
+	 (s-split "")
+	 (--map (gethash it cangjie--abc-to-han-hash))
+	 (s-join ""))))
 
 ;;;###autoload
 (defun cangjie (character)
