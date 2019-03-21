ae5470ed241d57c32fe4e2c58a7e9e39e066d519
Kisaragi Hiu <mail@kisaragi-hiu.com>
2019-03-22 05:07:53 +0900

    bump version

--- cangjie.el
+++ cangjie.el
@@ -2,7 +2,7 @@
 
 ;; Authors: Kisaragi Hiu <mail@kisaragi-hiu.com>
 ;; URL: https://github.com/kisaragi-hiu/cangjie.el
-;; Version: 0.7.0
+;; Version: 0.7.1
 ;; Package-Requires: ((emacs "24.4") (s "1.12.0") (dash "2.14.1") (f "0.2.0"))
 ;; Keywords: convenience, writing
 
